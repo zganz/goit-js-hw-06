dfdeeaff3bbad0d0ebd1f73875a7d0b1a95d7102
zganz <humanmailer@gmail.com>
2022-04-22 17:30:40 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
+console.log(formEl);
+
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Поля должны быть заполнены!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
 
